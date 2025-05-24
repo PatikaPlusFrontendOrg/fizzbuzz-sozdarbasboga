ed7ca7d0b09a27581a5b7ea572f3d5cca1c3abe5
Sözdar Başboğa <sozdarbasboga@Sozdar-MacBook-Air.local>
2025-05-24 22:46:08 +0300

    proje düzeltildi.

--- src/fizzbuzzSwitch.js
+++ src/fizzbuzzSwitch.js
@@ -1,5 +1,15 @@
 function fizzbuzzSwitch(num) {
-    // Kodlar buraya gelecek
+    switch (true) {
+        case num % 3 === 0 && num % 5 === 0:
+            return 'FizzBuzz'
+        case num % 5 === 0:
+            return 'Buzz'
+        case num % 3 === 0:
+            return 'Fizz'
+        
+        default:
+            return num;
+    }
 }
 
 module.exports = fizzbuzzSwitch;
